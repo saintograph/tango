3a9c4b1c76a27ca1df28fb2cef4270c67ac251a7
winfredselwyn <winfredselwyn@gmail.com>
2017-03-10 09:18:49 +0800

    initial commit

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+
+Meteor.startup(() => {
+  ReactDOM.render(
+    <App/>,
+    document.querySelector('.render-target')
+  );
+});
